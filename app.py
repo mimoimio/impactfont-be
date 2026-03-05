0c9498803beb5e77905a9c2fd74cd959aac13fc3
Mior Muhammad Adib bin Ahmad Zaha <129141433+mimoimio@users.noreply.github.com>
2026-03-05 20:42:21 +0800

    Host

--- app.py
+++ app.py
@@ -133,5 +133,5 @@ def generate_meme():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
-    app.run(debug=False, port=5000)
